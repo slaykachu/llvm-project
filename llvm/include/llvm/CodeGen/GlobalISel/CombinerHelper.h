a6417a57bce34022e6a64afb111bfa45f2fb2f98
Jacob Young <jacobly0@users.noreply.github.com>
2020-08-19 19:13:40 -0400

    [Combiner] Implement ext/trunc combine optimization.

--- llvm/include/llvm/CodeGen/GlobalISel/CombinerHelper.h
+++ llvm/include/llvm/CodeGen/GlobalISel/CombinerHelper.h
@@ -53,6 +53,12 @@ struct IndexedLoadStoreMatchInfo {
   bool IsPre;
 };
 
+struct ExtOrTrunc {
+  unsigned ExtOpc;
+  unsigned InPlaceOpc;
+  Register SrcReg;
+};
+
 struct PtrAddChain {
   int64_t Imm;
   Register Reg;
@@ -391,6 +397,9 @@ public:
 
   bool matchCombineIdentity(MachineInstr &MI);
   bool applyCombineIdentity(MachineInstr &MI);
+
+  bool matchCombineExtOrTrunc(MachineInstr &MI, ExtOrTrunc &Op);
+  void applyCombineExtOrTrunc(MachineInstr &MI, const ExtOrTrunc &Op);
 
   bool matchNarrowOp(MachineInstr &MI);
   void applyNarrowOp(MachineInstr &MI);
