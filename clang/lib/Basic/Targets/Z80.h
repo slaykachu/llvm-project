25c0f4c1564b698a998e5a113f2cdb080db5ba79
Jacob Young <jacobly0@users.noreply.github.com>
2020-09-22 04:27:57 -0400

    [Clang] [Z80] Fix formatting.

--- clang/lib/Basic/Targets/Z80.h
+++ clang/lib/Basic/Targets/Z80.h
@@ -28,7 +28,7 @@ public:
     TLSSupported = false;
     PointerAlign = BoolAlign = ShortAlign = IntAlign = HalfAlign = FloatAlign =
         DoubleAlign = LongDoubleAlign = LongAlign = LongLongAlign =
-        SuitableAlign = MinGlobalAlign = 8;
+            SuitableAlign = MinGlobalAlign = 8;
     DoubleWidth = 32;
     DoubleFormat = &llvm::APFloat::IEEEsingle();
     DefaultAlignForAttributeAligned = 32;
@@ -66,15 +66,16 @@ public:
   explicit Z80TargetInfo(const llvm::Triple &T, const TargetOptions &Opts)
       : Z80TargetInfoBase(T, Opts) {
     PointerWidth = IntWidth = 16;
-    resetDataLayout("e-m:o-p:16:8-p1:8:8-i16:8-i24:8-i32:8-i48:8-i64:8-i96:8-f32:8-f64:8-a:8-n8:16-S8");
+    resetDataLayout("e-m:o-p:16:8-p1:8:8-i16:8-i24:8-i32:8-i48:8-i64:8-i96:8-"
+                    "f32:8-f64:8-a:8-n8:16-S8");
   }
 
 private:
   bool setCPU(const std::string &Name) override;
   bool
-    initFeatureMap(llvm::StringMap<bool> &Features, DiagnosticsEngine &Diags,
-                   StringRef CPU,
-                   const std::vector<std::string> &FeaturesVec) const override;
+  initFeatureMap(llvm::StringMap<bool> &Features, DiagnosticsEngine &Diags,
+                 StringRef CPU,
+                 const std::vector<std::string> &FeaturesVec) const override;
   void getTargetDefines(const LangOptions &Opts,
                         MacroBuilder &Builder) const override;
   ArrayRef<const char *> getGCCRegNames() const override;
@@ -86,10 +87,12 @@ public:
       : Z80TargetInfoBase(T, Opts) {
     if (T.getEnvironment() == llvm::Triple::CODE16) {
       PointerWidth = IntWidth = 16;
-      resetDataLayout("e-m:o-p:16:8-p1:16:8-p2:24:8-i16:8-i24:8-i32:8-i48:8-i64:8-i96:8-f32:8-f64:8-a:8-n8:16-S8");
+      resetDataLayout("e-m:o-p:16:8-p1:16:8-p2:24:8-i16:8-i24:8-i32:8-i48:8-"
+                      "i64:8-i96:8-f32:8-f64:8-a:8-n8:16-S8");
     } else {
       PointerWidth = IntWidth = 24;
-      resetDataLayout("e-m:o-p:24:8-p1:16:8-p2:16:8-i16:8-i24:8-i32:8-i48:8-i64:8-i96:8-f32:8-f64:8-a:8-n8:16:24-S8");
+      resetDataLayout("e-m:o-p:24:8-p1:16:8-p2:16:8-i16:8-i24:8-i32:8-i48:8-"
+                      "i64:8-i96:8-f32:8-f64:8-a:8-n8:16:24-S8");
     }
   }
 
